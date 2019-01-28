6d252a27e938492f425a8ab316e07fd23ccb8855
armocida <armocidanicholas@gmail.com>
2019-01-27 21:24:45 -0600

    good example of multiple switch statement

---
+++ multipleSwitch.js
@@ -0,0 +1,34 @@
+/* A good example of a multipe switch statement with break statements
+* This switch statement returns the average salary of a person based on
+* their type of completed education.
+*/
+
+var education = "a Master's degree";
+
+var salary;
+
+switch (education) {
+  case "no high school diploma":
+    salary ="$25,636";
+    break;
+  case "a high school diploma":
+    salary ="$35,256";
+    break;
+  case"an Associate's degree" :
+    salary ="$41,496";
+    break;
+  case "a Bachelor's degree":
+    salary ="$59,124";
+    break;
+  case "a Master's degree": 
+    salary ="$69,732";
+    break;
+  case "a Professional degree":
+    salary ="$89,960";
+    break;
+  case "a Doctoral degree":
+    salary="$84,396";
+    break;
+}
+
+console.log("In 2015, a person with "+ education +" earned an average of "+ salary+"/year.");
