0391febd5f33eab9008b0974bc298de87fdbd44d
armocida <armocidanicholas@gmail.com>
2019-01-29 22:51:54 -0600

    logical NOT expression example

--- logicalNotExpression.js
+++ logicalNotExpression.js
@@ -1,4 +1,5 @@
-/* Logical Expressions are similar to mathmatical expressions, except Logical
+/** 
+* Logical Expressions are similar to mathmatical expressions, except Logical
 * expressions evaluate to either true or false.
 */
 
@@ -6,7 +7,8 @@
 
 // returns: true
 
-/* You've already seen logical expressions when you write comparisions.
+/** 
+* You've already seen logical expressions when you write comparisions.
 * A comparision is just a simple logical expression.
 * Similiar to mathmatical expressions that use +, -, *, / and %, there are
 * logical operators &&, ||, and ! that you can use to create more complex
