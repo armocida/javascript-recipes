6102434baa5eb000461b075fa039c4a683d383ee
armocida <armocidanicholas@gmail.com>
2019-01-31 23:05:32 -0600

    example of a basic for loop

--- basicForLoop.js
+++ basicForLoop.js
@@ -4,7 +4,7 @@
  * Example of a for loop that prints out the values from 0 to 5. Notice the
  * semicolons seperating the different statements of the for loops:
  * var i = 0; i < 6; i = 1 + 1
-*/
+ */
 
 for (var i = 0; i < 6; i = i + 1) {
     console.log("Printing out i = " + i);
@@ -18,10 +18,10 @@ for (var i = 0; i < 6; i = i + 1) {
  * Printing out i = 3
  * Printing out i = 4
  * Printing out i = 5
-*/
+ */
 
 /**
  * The for loop explicitly forces you to define the start point, stop point,
  * & each step of the loop. In fact, you'll get an Uncaught SyntaxError:
  * Unexpected token ) if you leave out any of the three required pieces.
-*/
+ */
