701ff8cbb78559e85a31477e4d1bc59c76933e76
armocida <armocidanicholas@gmail.com>
2019-01-31 23:02:55 -0600

    for loop having factorials

---
+++ havingFactorialsForLoop.js
@@ -0,0 +1,12 @@
+// a neat for loop having factorials
+
+solution = 12;
+for (var x = 1; x < 12; x++){
+    solution *= x;
+}
+    console.log(solution);
+
+/**
+ * A factorial is calculated by multiplying a number by all the numbers below it.
+ * For instance, 3! or "3 factorial" is 3*2*1=6
+ */
