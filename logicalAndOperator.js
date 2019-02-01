5e1f36e29a4caffc49bc45351cafaeca2ddc7f50
armocida <armocidanicholas@gmail.com>
2019-01-31 23:13:18 -0600

    logical AND operator example

--- logicalAndOperator.js
+++ logicalAndOperator.js
@@ -1,9 +1,9 @@
 /** 
-* The && symbol is the logical AND operator and it is ued to combine two logical
-* expressions into one larger logical expression. If both smaller expressions are
-* true, then the entire expression evaluates to true. If either one of teh smalller
-* expressions is false, then the whole logical expression is false.
-*/
+ * The && symbol is the logical AND operator and it is ued to combine two logical
+ * expressions into one larger logical expression. If both smaller expressions are
+ * true, then the entire expression evaluates to true. If either one of teh smalller
+ * expressions is false, then the whole logical expression is false.
+ */
 
 var colt = "not busy";
 var weather = "nice";
@@ -15,7 +15,7 @@ if (colt === "not busy" && weather === "nice") {
 // prints: go to the park
 
 /** 
-* Another way to think about it is when the && operator is placed between the two
-* statements, the code literally reads, "if cold is not busy AND the weather is nice,
-* then go to the park".
-*/
+ * Another way to think about it is when the && operator is placed between the two
+ * statements, the code literally reads, "if cold is not busy AND the weather is nice,
+ * then go to the park".
+ */
