1d28b03fe6acecd472e7d7b3764fc9ed536d4139
armocida <armocidanicholas@gmail.com>
2019-02-01 19:35:21 -0600

    very basic function no arguments

---
+++ veryBasicFunction.js
@@ -0,0 +1,12 @@
+// example of a very basic function with no arguments
+
+/**
+ * Declare a function called laugh() that returns "hahahahahahahaha!".
+ * Print the value returned from the laugh() function to the console.
+ */
+
+ function laugh() {
+return "hahahahahahahaha!"
+ }
+
+ console.log(laugh());
