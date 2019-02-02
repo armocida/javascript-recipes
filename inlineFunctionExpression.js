2f440097c8251d4d89eda8015d2bc50a51b17248
armocida <armocidanicholas@gmail.com>
2019-02-02 17:53:09 -0600

    inline function expression

---
+++ inlineFunctionExpression.js
@@ -0,0 +1,14 @@
+// good example of an inline function expression
+
+// Function declaration that takes in two arguments: a function for displaying
+// a message, along with a name of a movie
+function movies(messageFunction, name) {
+    messageFunction(name);
+}
+
+// Call the movies function, pass in the function and name of movie
+movies(function displayFavorite(movieName){
+    console.log("My favorite movie is " + movieName);
+}, "Finding Nemo");
+
+// returns: My favorite movie is Finding Nemo
