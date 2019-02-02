0943d63773b616e71865f77f5fb2f2dac7e8dc6e
armocida <armocidanicholas@gmail.com>
2019-02-02 17:26:46 -0600

    example basic callback function

--- callbackFunction.js
+++ callbackFunction.js
@@ -1,5 +1,12 @@
 // good example of a callback function
 
+/**
+ * Let's say you had a helloCat() function, and you wanted it to return "Hello"
+ * followed by a string of "meows" like you had with catSays. Rather than redoing
+ * all of your hardwork, you can make helloCat() accept a callbacl function, and
+ * pass in catSays.
+ */
+
 // function expression catSays
 var catSays = function(max) {
     var catMessage = "";
@@ -16,3 +23,7 @@ function helloCat(callbackFunc) {
 
 // pass in catSays as a callback function
 helloCat(catSays);
+
+/**
+ * A function that is passed into another function is called a callback.
+ */
