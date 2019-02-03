5c1777c001ea3cab8ca46774369b26cf445fc8d5
armocida <armocidanicholas@gmail.com>
2019-02-02 18:20:49 -0600

    anonymous inline function

---
+++ anonymousInlineFunction.js
@@ -0,0 +1,15 @@
+// basic example of an anonymous inline function
+
+/**
+ * Write an anonymous function expression that stores a function in a variable
+ * called "laugh" and outputs the number of ha's that you pass in as an argument.
+ */
+
+var laugh = function(num){
+var string="";
+    for (x = 0; x < num; x++){
+    string+="ha";
+    }
+    return string +"!";
+};
+console.log(laugh(10));
